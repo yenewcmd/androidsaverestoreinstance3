14783690a099c5ecaf5a53bd98b9dd0cf6212ed2
yenewcmd <76645571+yenewcmd@users.noreply.github.com>
2020-12-27 13:03:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/androidsaverestoreinstance/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.androidsaverestoreinstance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    TextView textView;
+    int counter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            String message = savedInstanceState.getString("message");
+            //Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+
+            counter = savedInstanceState.getInt("counter", 0);
+        }
+
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        textView.setText(String.valueOf(counter));
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter = Integer.valueOf(textView.getText().toString()) + 1;
+                textView.setText(String.valueOf(counter));
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("message", "This is a saved message");
+        outState.putInt("counter", counter);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Toast.makeText(getApplicationContext(), "onRestoreInstanceState", Toast.LENGTH_SHORT).show();
+        counter = savedInstanceState.getInt("counter", 0);
+
+    }
+
+}
